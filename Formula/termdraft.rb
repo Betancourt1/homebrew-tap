1688897711a2f9ec23d637fb9853becefde2832e
Fernando Betancourt <fernandobetancourtmoreno@gmail.com>
2026-07-20 02:07:02 -0600

    termdraft 0.4.0

--- Formula/termdraft.rb
+++ Formula/termdraft.rb
@@ -1,9 +1,10 @@
 class Termdraft < Formula
   desc "Local-first Markdown editor for the terminal"
   homepage "https://github.com/Betancourt1/TermDraft"
-  url "https://github.com/Betancourt1/TermDraft/archive/refs/tags/v2.0.0.tar.gz"
-  sha256 "4b8b35ce2051e96a8cb68b6ee84f48145d7df3fc5b5e25076faca3a1f63186d0"
+  url "https://github.com/Betancourt1/TermDraft/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "7078cb44d96caa14df7aa6435c3f0fdf4e906f5d2fad1a135bc2e357380c52ff"
   license "MIT"
+  version_scheme 1
 
   depends_on "rust" => :build
 
